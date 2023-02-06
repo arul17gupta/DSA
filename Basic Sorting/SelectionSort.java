5bc772d5421dfb12523c0e68386bda14c7fada9a
Arul Gupta <arul17gupta@gmail.com>
2023-02-06 15:39:22 +0530

    Bubble Sort

--- Basic Sorting/SelectionSort.java
+++ Basic Sorting/SelectionSort.java
@@ -1,18 +1,8 @@
 import java.util.*;
 
-
-public class bubbleSort {
-
+public class SelectionSort {
     public static void Sort(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+
     }
 
     public static void Print(int arr[]) {
@@ -23,13 +13,11 @@ public class bubbleSort {
     }
 
     public static void main(String[] args) {
-        System.out.println("Enter the size of array: ");
+        System.out.println("Enter the Size of array");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println("Enter the Elements: ");
-
+        System.out.println("Enter the Elements of array: ");
         int[] arr = new int[n];
-
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
