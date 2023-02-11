54bb433b67e8d95da0d42f2a11038ecc9bce2292
Arul Gupta <arul17gupta@gmail.com>
2023-02-11 22:12:53 +0530

    Basic Sorting Complete

--- Basic Sorting/InsertionSort.java
+++ Basic Sorting/InsertionSort.java
@@ -1,21 +1,23 @@
 import java.util.*;
 
-public class bubbleSort {
+public class InsertionSort {
 
     public static void Sort(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        for (int i = 1; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev--;
+
             }
+            arr[prev + 1] = curr;
         }
+
     }
 
     public static void Print(int arr[]) {
-        System.out.println("The Sorted Array : ");
+        System.out.println("The Sorted Array is");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
@@ -25,15 +27,13 @@ public class bubbleSort {
         System.out.println("Enter the size of array: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        System.out.println("Enter the Elements: ");
-
+        System.out.println("Enter the Elements of the array:");
         int[] arr = new int[n];
-
         for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
+           arr[i] = sc.nextInt();
         }
-        Sort(arr);
+        // Sort(arr);
+        Arrays.sort(arr);
         Print(arr);
-
     }
 }
--- Basic Sorting/SelectionSort.java
+++ Basic Sorting/SelectionSort.java
@@ -2,7 +2,18 @@ import java.util.*;
 
 public class SelectionSort {
     public static void Sort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
 
+                }
+            }
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
     }
 
     public static void Print(int arr[]) {
